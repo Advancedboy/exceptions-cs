c6472115a56e9599c67ab1f78dcb28cf1ca0c279
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-06-03 17:54:56 +0300

    Added project files

---
+++ MyOwnException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class MyOwnException : Exception
+    {
+        public MyOwnException() : base("This is my Exception")
+        {
+        }
+
+        public MyOwnException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine($"Интовый {result}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Введите целое число");
+                }
+            }
+            int i = 5;
+            try
+            {
+                //var a = 5;
+                //var b = a / 0;
+                //throw new DivideByZeroException("test");
+                throw new MyOwnException();
+            }
+            catch(MyOwnException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex) when (i == 5)
+            {
+                Console.WriteLine("i = 5");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Деление на ноль");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Какое-то исключение....");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Работа завершена");
+                Console.ReadLine();
+            }
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+        }
+    }
+}
